3bed926311781c820a4d80b00f1ac76234ac8f8b
bazen <bazenatlas@gmail.com>
2023-11-15 15:33:04 -0500

    more scaffolding

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,11 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <PageWrapper>
-      <div className='flex flex-col lg:flex-row'>
-        <div className='flex flex-col pr-4 w-full lg:w-[37vw] xl:w-[27vw] lg:fixed'>
+      <div className='flex flex-col max-w-xl lg:max-w-none mx-auto lg:flex-row'>
+        <div className='flex flex-col lg:pr-6 w-full xl:w-[27vw] xl:fixed'>
           <div className='py-6 border-2 border-black/10 rounded-md mt-4 flex gap-4 px-4 items-center'>
-            <div className='bg-black/10 rounded-full h-20 w-20 mr-6'></div>
-            <p className='max-w-sm'>
+            <div className='bg-black/10 rounded-full h-20 w-20 block'></div>
+            <p className='max-w-sm w-[80%] ml-auto'>
               This is a bio about someone who works in tech. This is the second
               sentence.
             </p>
@@ -19,7 +19,7 @@ export default function Home() {
             <div className='w-full text-center bg-white ml-1 py-1 rounded-sm'>
               Projects
             </div>
-            <div className='w-full text-center  mx-1 py-1 rounded-sm'>
+            <div className='w-full text-center mx-1 py-1 rounded-sm'>
               Services
             </div>
           </div>
@@ -28,7 +28,8 @@ export default function Home() {
             <Pill key={i} />
           ))}
         </div>
-        <div className='w-full h-[35vh] md:h-[60vh] lg:mt-4 lg:h-[91vh] lg:w-[60vw] lg:fixed right-6 lg:ml-auto xl:w-[70.5vw] aspect-square border-2 border-black/10 rounded-md'></div>
+
+        <div className='w-full h-[35vh] lg:h-[91vh] lg:mt-4 xl:fixed xl:right-6 2xl:h-[91vh] lg:ml-auto xl:w-[70.5vw] aspect-square border-2 border-black/10 rounded-md'></div>
       </div>
     </PageWrapper>
   )
